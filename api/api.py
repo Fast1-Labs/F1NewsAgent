1f5b4d8b73e302e3d4760b046697fd17a7e4f91d
anilyavas <anil.yavas99@gmail.com>
2024-12-31 16:22:09 +0300

    updated api

--- api/api.py
+++ api/api.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-
-from api.news_agent import get_f1_news
+from news_agent import get_f1_news
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the F1 News API"}
-
-
-@app.get("/news")
-def fetch_news():
+@app.get("/f1-news")
+async def fetch_news():
     news = get_f1_news()
-    return {"news": news}
+    return {"status": "success", "data": news}
